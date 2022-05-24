64c56afb87e20a36e54c2839fd4e55a0c0e5a6b5
Luis Gabriel <janco7249@gmail.com>
2022-05-24 15:10:10 -0400

    feat(Root): Setup MyProject settings

---
+++ GraphicsPractice/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' La información general de un ensamblado se controla mediante el siguiente 
+' conjunto de atributos. Cambie estos valores de atributo para modificar la información
+' asociada con un ensamblado.
+
+' Revisar los valores de los atributos del ensamblado
+
+<Assembly: AssemblyTitle("Práctico de Gráficos - Luis Gabriel J. A.")>
+<Assembly: AssemblyDescription("Este programa gráfica lineas simetricas y lineas aleatorias en paralelo. Práctico del Ing. Alberto Mollo.")>
+<Assembly: AssemblyCompany("Luis Gabriel Janco")>
+<Assembly: AssemblyProduct("Práctico de Gráficos")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+<Assembly: Guid("2616d8fa-36b3-4ae2-929d-ca3e8e5bed3b")>
+
+' La información de versión de un ensamblado consta de los cuatro valores siguientes:
+'
+'      Versión principal
+'      Versión secundaria
+'      Número de compilación
+'      Revisión
+'
+' Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+' mediante el carácter '*', como se muestra a continuación:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
